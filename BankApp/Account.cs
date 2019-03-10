a7ff5bf7b45cf835508d988780e29c9be4664e64
mugdha pati <mugdhapati@gmail.com>
2019-03-10 11:25:00 -0700

    Methods

--- BankApp/Account.cs
+++ BankApp/Account.cs
@@ -4,12 +4,46 @@ using System.Text;
 
 namespace BankApp
 {
+    /// <summary>
+    /// Account that represents
+    /// BANK ACCOUNT here you can
+    /// withdaraw or deposit money
+    /// </summary>
     class Account
     {
+        #region Properties
+        /// <summary>
+        /// unique number for the account
+        /// </summary>
         public int AccountNumber { get; set; }
+        /// <summary>
+        ///  email address of the account holder
+        /// </summary>
         public string EmailAddress { get; set; }
         public decimal Balance { get; set; }
         public string AccountType { get; set; }
         public DateTime CreatedDate { get; set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        public void Deposit (decimal amount)
+        {
+            //Balance = Balance + amount;
+            Balance += amount; 
+        }
+        /// <summary>
+        /// Withdraw money from account
+        /// </summary>
+        /// <param name="amount"></param>
+        public void Withdraw (decimal amount)
+        {
+
+            Balance -= amount;
+        }
+        #endregion
     }
 }
--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -6,7 +6,7 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new Account();
         }
     }
 }
