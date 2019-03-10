739af429a3132bf5086568622089519134d75699
mugdha pati <mugdhapati@gmail.com>
2019-03-10 10:47:15 -0700

    Initial Commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    class Account
+    {
+        public int AccountNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal Balance { get; set; }
+        public string AccountType { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
