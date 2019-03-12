3808bbd72326680afee21e5662c0d41893ee90f8
mugdha pati <mugdhapati@gmail.com>
2019-03-11 18:24:28 -0700

    Latest Changes

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -7,6 +7,8 @@ namespace BankApp
         static void Main(string[] args)
         {
             var account = new Account();
+            account.AccountNumber = 12345;
+            account.Balance = 10000;
         }
     }
 }
